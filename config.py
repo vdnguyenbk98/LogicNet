8964bbd83dac906d2920aae7c135e1757d5c22be
root <root@logicnet1.datacrunch.io>
2024-10-22 03:42:17 +0000

    Fix

---
+++ config.py
@@ -0,0 +1,4 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+URL_API = "http://localhost:8000/ask"
--- logicnet/miner/forward.py
+++ logicnet/miner/forward.py
@@ -3,45 +3,53 @@ import openai
 import bittensor as bt
 import traceback
 import re
-
+from config import URL_API
+import httpx
 
 async def solve(
-    synapse: LogicSynapse, openai_client: openai.AsyncOpenAI, model: str
+    synapse: LogicSynapse,
 ) -> LogicSynapse:
     try:
-        bt.logging.info(f"Received synapse: {synapse}")
         logic_question: str = synapse.logic_question
-        messages = [
-            {"role": "user", "content": logic_question},
-        ]
-        response = await openai_client.chat.completions.create(
-            model=model,
-            messages=messages,
-            max_tokens=2048,
-            temperature=0.8,
-        )
-        synapse.logic_reasoning = response.choices[0].message.content
+        # messages = [
+        #     {"role": "user", "content": logic_question},
+        # ]
+        # response = await openai_client.chat.completions.create(
+        #     model=model,
+        #     messages=messages,
+        #     max_tokens=2048,
+        #     temperature=0.8,
+        # )
+        # synapse.logic_reasoning = response.choices[0].message.content
 
-        messages.extend(
-            [
-                {"role": "assistant", "content": synapse.logic_reasoning},
-                {
-                    "role": "user",
-                    "content": "Give me the final short answer as a sentence. Don't reasoning anymore, just say the final answer in math latex.",
-                },
-            ]
-        )
+        # messages.extend(
+        #     [
+        #         {"role": "assistant", "content": synapse.logic_reasoning},
+        #         {
+        #             "role": "user",
+        #             "content": "Give me the final short answer as a sentence. Don't reasoning anymore, just say the final answer in math latex.",
+        #         },
+        #     ]
+        # )
 
-        response = await openai_client.chat.completions.create(
-            model=model,
-            messages=messages,
-            max_tokens=512,
-            temperature=0.7,
-        )
-        synapse.logic_answer = response.choices[0].message.content
+        # response = await openai_client.chat.completions.create(
+        #     model=model,
+        #     messages=messages,
+        #     max_tokens=512,
+        #     temperature=0.7,
+        # )
+        # synapse.logic_answer = response.choices[0].message.content
+        
+        async with httpx.AsyncClient(timeout=50) as client:
+            print("---------:", URL_API)
+            response = await client.post(
+                URL_API, 
+                json={"question": logic_question}
+            )
+        response=  response.json()
+        synapse.logic_answer =response['answer']
+        synapse.logic_reasoning = response["reasoning"]
 
-        bt.logging.info(f"Logic answer: {synapse.logic_answer}")
-        bt.logging.info(f"Logic reasoning: {synapse.logic_reasoning}")
         return synapse
     except Exception as e:
         bt.logging.error(f"Error in forward: {e}")
