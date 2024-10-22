ad034875479429237171bdf993fb887c8304d802
root <root@logicnet1.datacrunch.io>
2024-10-22 03:47:46 +0000

    Fix

--- neurons/miner/miner.py
+++ neurons/miner/miner.py
@@ -25,7 +25,7 @@ class Miner(BaseMinerNeuron):
         }
         self.num_processing_requests = 0
         self.total_request_in_interval = 0
-        bt.logging.info(f"\033[1;32m🧠 Miner info: {self.miner_info}\033[0m")
+        bt.logging.info(f"Miner info: {self.miner_info}")
         self.openai_client = openai.AsyncOpenAI(
             base_url=self.config.miner.llm_client.base_url,
             api_key=self.config.miner.llm_client.key,
@@ -36,25 +36,14 @@ class Miner(BaseMinerNeuron):
         Forward pass for the miner neuron. This function is called when a synapse is received by the miner neuron.
         By default, Miner will utilize the LLM API to solve the logic problem.
         """
-        start_time = time.time()
         try:
+            synapse = await solve(synapse)
             self.num_processing_requests += 1
-            bt.logging.info(f"\033[1;33;44m🚀 Start processing request {self.num_processing_requests}\033[0m")
-            synapse = await solve(
-                synapse=synapse,
-                openai_client=self.openai_client,
-                model=self.config.miner.llm_client.model,
-            )
             self.total_request_in_interval += 1
-            
         except Exception as e:
-            bt.logging.error(f"\033[1;31m❌ Error in forward: {e}\033[0m")
+            bt.logging.error(f"Error in forward: {e}")
             traceback.print_exc()
-    
-        finally:
-            process_time = time.time() - start_time
-            bt.logging.info(f"\033[1;34;47m✅ Served request {self.num_processing_requests}: {round(process_time,2)} seconds\033[0m")
-            
+
         return synapse
 
     async def forward_info(self, synapse: Information) -> Information:
@@ -70,7 +59,7 @@ class Miner(BaseMinerNeuron):
             if synapse.dendrite.hotkey not in self.metagraph.hotkeys:
                 # Ignore requests from unrecognized entities.
                 bt.logging.trace(
-                    f"\033[1;35m🛑 Blacklisting unrecognized hotkey {synapse.dendrite.hotkey}\033[0m"
+                    f"Blacklisting unrecognized hotkey {synapse.dendrite.hotkey}"
                 )
                 return True, "Unrecognized hotkey"
 
@@ -79,7 +68,7 @@ class Miner(BaseMinerNeuron):
 
             if validator_uid not in self.volume_per_validator:
                 bt.logging.trace(
-                    f"\033[1;35m🛑 Blacklisting {validator_uid}-validator has {stake} stake\033[0m"
+                    f"Blacklisting {validator_uid}-validator has {stake} stake"
                 )
                 return True, "Not enough stake"
             if logicnet.miner.check_limit(
@@ -90,13 +79,13 @@ class Miner(BaseMinerNeuron):
                 interval=self.config.miner.limit_interval,
             ):
                 bt.logging.trace(
-                    f"\033[1;35m🛑 Blacklisting {validator_uid}-validator for exceeding the limit\033[0m"
+                    f"Blacklisting {validator_uid}-validator for exceeding the limit"
                 )
                 return True, "Limit exceeded"
 
             return False, "All passed!"
         except Exception as e:
-            bt.logging.error(f"\033[1;31m❌ Error in blacklist: {e}\033[0m")
+            bt.logging.error(f"Error in blacklist: {e}")
             traceback.print_exc()
             return False, "All passed!"
 
@@ -108,7 +97,7 @@ class Miner(BaseMinerNeuron):
             self.metagraph.S[caller_uid]
         )  # Return the stake as the priority.
         bt.logging.trace(
-            f"\033[1;36m🔝 Prioritizing {synapse.dendrite.hotkey} with value: {priority}\033[0m"
+            f"Prioritizing {synapse.dendrite.hotkey} with value: ", priority
         )
         return priority
 
@@ -117,10 +106,10 @@ if __name__ == "__main__":
     with Miner() as miner:
         start_time = time.time()
         while True:
-            bt.logging.info(f"\033[1;32m⛏️ Miner running... {time.time()}\033[0m")
+            bt.logging.info("Miner running...", time.time())
             if time.time() - start_time > 300:
                 bt.logging.info(
-                    f"\033[1;32m---Total request in last 5 minutes: {miner.total_request_in_interval}\033[0m"
+                    f"---Total request in last 5 minutes: {miner.total_request_in_interval}"
                 )
                 start_time = time.time()
                 miner.total_request_in_interval = 0
@@ -133,5 +122,5 @@ if __name__ == "__main__":
                     )
                 )
             except Exception as e:
-                bt.logging.error(f"\033[1;31m❌ Error updating volume per validator: {e}\033[0m")
+                print(e)
             time.sleep(60)
